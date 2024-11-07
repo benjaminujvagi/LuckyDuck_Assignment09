78bf59a8826df6e969b826a7240d07ec5a07d03c
Jacob Schulte <schul2jt@mail.uc.edu>
2024-11-06 19:44:47 -0500

    query.py

--- LuckyDuck_Assignment09/NewFolder2/module2.py
+++ LuckyDuck_Assignment09/NewFolder2/module2.py
@@ -1,1 +1,13 @@
+#query.py
 
+def get_products(cursor):
+    cursor.execute("SELECT ProductID, [UPC-A], Description, ManufacturerID, BrandID FROM tProduct")
+    return cursor.fetchall()
+
+def get_manufacturer(cursor, manufacturer_id):
+    cursor.execute(f"SELECT Manufacturer FROM tManufacturer WHERE ManufacturerID = {manufacturer_id}")
+    return cursor.fetchone()[0]
+
+def get_brand(cursor, brand_id):
+    cursor.execute(f"SELECT Brand FROM tBrand WHERE BrandID = {brand_id}")
+    return cursor.fetchone()[0]
