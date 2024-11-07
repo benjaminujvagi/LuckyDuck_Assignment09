2cdb3130180d4cf536d88bfd1a64efb3f348dcfc
shawdp <shawdp@mail.uc.edu>
2024-11-07 07:08:25 -0500

    connection package #3

--- LuckyDuck_Assignment09/NewFolder1/module1.py
+++ LuckyDuck_Assignment09/NewFolder1/module1.py
@@ -2,16 +2,17 @@
 import pyodbc
 
 def get_connection():
-    conn = None
+    """
+    Connect to grocery store database
+    """
     try:
         conn = pyodbc.connect(
             'Driver={SQL Server};'
             'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
             'Database=GroceryStoreSimulator;'
             'uid=IS4010Login;'
-            'pwd=P@ssword2;'
-        )
+            'pwd=P@ssword2;')
         print("Connection successful")
-    except pyodbc.Error as e:
-        print(f"Error accessing database: {e}")
+    except:
+        conn = None
     return conn
