29973f8bdc011b19ce1f1661caec2fd1735c3e04
shawdp <shawdp@mail.uc.edu>
2024-11-06 19:44:07 -0500

    Connection package

--- LuckyDuck_Assignment09/NewFolder1/module1.py
+++ LuckyDuck_Assignment09/NewFolder1/module1.py
@@ -1,1 +1,27 @@
+#connection.py
 
+import pyodbc
+
+def get_connection():
+
+    try:
+
+        conn = pyodbc.connect('Driver={SQL Server};'
+
+                          'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+
+                          'Database=GroceryStoreSimulator;'
+
+                          'uid=IS4010Login;'
+
+                          'pwd=P@ssword2;')
+
+        print("Connection successful")
+
+        return conn
+
+    except pyodbc.Error as e:
+
+        print("Error accessing database:", e)
+
+        return None
---
+++ LuckyDuck_Assignment09/mainPackage/main.py
@@ -0,0 +1,8 @@
+# main.py
+
+import pyodbc
+conn = pyodbc.connect('Driver={SQL Server};'
+'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+'Database=IS4010;'
+'uid=IS4010Login;'
+'pwd=P@ssword2;')
