40dd7132a790884988c2f1d094f12ffc37ea58eb
shawdp <shawdp@mail.uc.edu>
2024-11-06 19:49:28 -0500

    Connection package #2

--- LuckyDuck_Assignment09/NewFolder1/module1.py
+++ LuckyDuck_Assignment09/NewFolder1/module1.py
@@ -1,27 +1,17 @@
-#connection.py
-
+# connection.py
 import pyodbc
 
 def get_connection():
-
+    conn = None
     try:
-
-        conn = pyodbc.connect('Driver={SQL Server};'
-
-                          'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
-
-                          'Database=GroceryStoreSimulator;'
-
-                          'uid=IS4010Login;'
-
-                          'pwd=P@ssword2;')
-
+        conn = pyodbc.connect(
+            'Driver={SQL Server};'
+            'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+            'Database=GroceryStoreSimulator;'
+            'uid=IS4010Login;'
+            'pwd=P@ssword2;'
+        )
         print("Connection successful")
-
-        return conn
-
     except pyodbc.Error as e:
-
-        print("Error accessing database:", e)
-
-        return None
+        print(f"Error accessing database: {e}")
+    return conn
